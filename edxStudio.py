cb695ffa6985aac57262cdc5929d2010a430d869
ichuang <ichuang@mit.edu>
2014-03-12 17:56:55 -0400

    initial version - mvp

---
+++ edxStudio.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+#
+# File:   edxStudio.py
+# Date:   12-Mar-14
+# Author: I. Chuang <ichuang@mit.edu>
+#
+# upload / download course .tar.gz files to/from edX Studio
+#
+# usage:  
+#
+# python edxStudio.py course_id tar_file_name
+#
+# Put username and passsword in config.py; eg
+#    username = "me"
+#    password = "my password"
+
+
+import os, sys
+import time
+import requests
+from config import *
+
+class edxStudio(object):
+
+    def __init__(self, base="https://studio.edx.org", username='', password=''):
+        self.ses = requests.session()
+        self.BASE = base
+        self.login(username, password)
+
+    def login(self, username, pw):
+        url = '%s/signin' % self.BASE
+        r1 = self.ses.get(url)
+        csrf = self.ses.cookies['csrftoken']
+        url2 = '%s/login_post' % self.BASE
+        headers = {'X-CSRFToken':csrf,
+                   'Referer': '%s/signin' % self.BASE}
+        r2 = self.ses.post(url2, data={'email': username, 'password': pw}, headers=headers)
+
+        if not r2.status_code==200:
+            print "Login failed!"
+            print r2.text
+    
+    def do_download(self, course_id):
+    
+        print "Downloading tar.gz for %s" % (course_id)
+    
+        (org, num, sem) = course_id.split('/')
+        url = '%s/export/%s/branch/draft/block/%s?_accept=application/x-tgz' % (self.BASE, course_id.replace('/','.'), sem)
+        r3 = self.ses.get(url)
+    
+        dt = time.ctime(time.time()).replace(' ','_').replace(':','')
+        fn = 'COURSE_DATA/COURSE-%s___%s.tar.gz' % (course_id.replace('/','__'),dt)
+    
+        open(fn, 'w').write(r3.content)
+        print "--> %s" % (fn)
+        return fn
+    
+    def do_upload(self, course_id, tfn, nwait=20):
+    
+        print "Uploading %s for %s" % (tfn, course_id)
+    
+        (org, num, sem) = course_id.split('/')
+        url = '%s/import/%s/branch/draft/block/%s' % (self.BASE, course_id.replace('/','.'), sem)
+    
+        files = {'course-data': (tfn, open(tfn, 'rb'), 'application/x-gzip')}
+    
+        csrf = self.ses.cookies['csrftoken']
+        print "csrf=%s" % csrf
+        headers = {'X-CSRFToken':csrf,
+                   'Referer': url,
+                   'Accept': 'application/json, text/javascript, */*; q=0.01',
+               }
+    
+        r2 = self.ses.get(url)
+        print r2.status_code
+        r3 = self.ses.post(url, files=files, headers=headers)
+        # print r3.headers
+        print url
+        print r3.status_code
+    
+        print "--> %s" % (r3.content)
+    
+        url = '%s/import_status/%s/branch/draft/block/%s/%s' % (self.BASE, course_id.replace('/','.'), sem, tfn.replace('/','-'))
+    
+        for k in range(nwait):
+            r4 = self.ses.get(url)
+            print r4.content
+            sys.stdout.flush()
+            time.sleep(2)
+            
+        dt = time.ctime(time.time()).replace(' ','_').replace(':','')
+
+
+#-----------------------------------------------------------------------------
+
+if __name__=='__main__':
+
+    if len(sys.argv)>2:
+        cid = sys.argv[1]
+        tfn = sys.argv[2]
+    else:
+        print "Usage: python %s course_id tar_file_name.tgz" % (sys.argv[0])
+        exit(0)
+
+    es = edxStudio(username=username, password=password)
+    es.do_upload(cid, tfn)
+    #es.do_download(cid)
+
+
---
+++ git2edx.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+#
+# File:   git2edx.py
+# Date:   12-Mar-14
+# Author: I. Chuang <ichuang@mit.edu>
+#
+# web server which provides git2edx service
+# uses wsgi
+#
+# configuration parameters in config.py
+
+import sys, string, re, time, os
+import cgi
+import json
+import urllib
+import requests
+import socket
+
+from cgi import parse_qs, escape
+from lxml import etree
+from edxStudio import edxStudio
+
+#-----------------------------------------------------------------------------
+# config
+
+config = {'username': "",
+          'password': "",
+          'ANYREPO': True,
+          'FORCE_BRANCH': '',
+          'REPODIR': '',
+          'LOGFILE': "",
+          'REPO2COURSE_MAP': {},	# if empty, then course id will be determined from repo XML contents
+          'PORT': 8121,
+          }
+
+CFN = 'config.json'
+if os.path.exists(CFN):
+    import json
+    new_config = json.loads(open(CFN).read())
+    config.update(new_config)
+
+#-----------------------------------------------------------------------------
+
+PIDFILE = "git2edx.pid"
+
+open(PIDFILE,'w').write(str(os.getpid()))
+
+def LOG(x):
+    fp = open(config['LOGFILE'],'a')
+    if type(x)==dict:
+        for k in x:
+            if not k:
+                continue
+            s = '  %s : %s' % (k,x[k])
+            fp.write(s)
+            print s
+    #if type(x)==type('str'):
+    else:
+        fp.write(x)
+        fp.write('\n')
+        print x
+
+    sys.stdout.flush()
+    fp.flush()
+    fp.close()
+
+#-----------------------------------------------------------------------------
+
+def upload_to_edx(rdir, repo):
+    '''
+    tar up repo, and upload to edX
+
+    rdir = directory with repo contents
+    repo = repo name
+    '''
+    
+    # get course_id
+    if not config['REPO2COURSE_MAP']:
+        cxml = etree.parse(open('%s/course.xml' % rdir)).getroot()
+        org = cxml.get('org')
+        course = cxml.get('course')
+        sem = cxml.get('url_name')
+        course_id = '/'.join([org, course, sem])
+    else:
+        course_id = config['REPO2COURSE_MAP'].get(repo, '')
+        
+    if not course_id:
+        LOG("Error: cannot determine course_id for repo=%s" % repo)
+        return
+    
+    # create tar.gz file
+    tfn = '%s.tar.gz' % rdir
+    os.chdir(rdir)
+    os.chdir('..')
+    cmd = "tar czf %s --exclude=.git --exclude=src %s" % (tfn, repo)
+    LOG(cmd)
+    LOG(os.popen(cmd).read())
+    
+    # upload to studio
+    LOG('-'*30 + "Uploading %s to edX studio course_id=%s" % (tfn, course_id))
+    es = edxStudio(username=config['username'], password=config['password'])
+    es.do_upload(course_id, tfn, nwait=3)
+
+#-----------------------------------------------------------------------------
+
+def do_git2edx(environ, start_response):
+
+    if True:
+
+        # the environment variable CONTENT_LENGTH may be empty or missing
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except (ValueError):
+            request_body_size = 0
+                   
+        # When the method is POST the query string will be sent
+        # in the HTTP request body which is passed by the WSGI server
+        # in the file like wsgi.input environment variable.
+        request_body = environ['wsgi.input'].read(request_body_size)
+        post = parse_qs(request_body)
+
+	LOG('-----------------------------------------------------------------------------')
+	LOG('connect at %s' % time.ctime(time.time()))
+
+	args = post
+
+        LOG('environ = %s' % environ)
+        LOG('post = %s' % post)
+        LOG('payload = "%s"' % args.get('payload','none'))
+
+	# from github
+	if 'payload' in args:
+	    gitargs = json.loads(args['payload'][0])
+	    repo = gitargs['repository']['name']
+	    LOG('repo = %s\n' % repo)
+
+            REPOS = os.listdir(config['REPODIR'])
+
+            if config['ANYREPO'] and repo not in REPOS:
+                os.chdir(config['REPODIR'])
+                # should do a git clone here...
+
+	    # if it is one of our repos, then try doing a git2edx on it
+	    if config['ANYREPO'] or repo in REPOS:
+		rdir = '%s/%s' % (config['REPODIR'],repo)
+		os.chdir(rdir)
+                # figure out which branch the directory is on
+                branch = ''
+                for k in os.popen('git branch').readlines():
+                    if k[0]=='*':
+                        branch = k[2:].strip()
+                LOG('Current branch = "%s"' % branch)
+
+                # get current branch
+                cmd = "git reset --hard HEAD; git clean -f -d; git pull origin"
+                LOG(cmd)
+		LOG(os.popen(cmd).read())
+
+                # get branch
+                cmd = "git reset --hard HEAD; git clean -f -d; git pull origin %s" % branch
+                LOG(cmd)
+		LOG(os.popen(cmd).read())
+
+                if config['FORCE_BRANCH']:
+                    cmd = 'git checkout %s' % config['FORCE_BRANCH']
+                    LOG(cmd)
+                    LOG(os.popen(cmd).read())
+                    
+		# tar it up and upload to edX studio site
+                upload_to_edx(rdir, repo)
+
+	    else:
+		LOG("unknown\n")
+
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return ['''Hello World - github''']
+        LOG("done at %s" % time.ctime(time.time()))
+
+if __name__ == '__main__':
+    LOG('========> started at %s' % time.ctime(time.time()))
+    from wsgiref.simple_server import make_server
+    import socket
+    host = socket.gethostname()
+    srv = make_server(host, PORT, do_git2edx)
+    srv.serve_forever()
+    
