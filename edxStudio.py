9b6dbc93d4369b01c0328c5cf686a271f1bed880
ichuang <ichuang@mit.edu>
2014-06-11 21:10:35 -0400

    modify edXstudio to handle new 'opaque keys' URLs used by edX Studio

--- edxStudio.py
+++ edxStudio.py
@@ -51,7 +51,11 @@ class edxStudio(object):
         (org, num, sem) = course_id.split('/')
         url = '%s/export/%s/branch/draft/block/%s?_accept=application/x-tgz' % (self.BASE, course_id.replace('/','.'), sem)
         r3 = self.ses.get(url)
-    
+
+        if not r3.ok or (r3.status_code==404):
+            url = '%s/export/slashes:%s+%s?_accept=application/x-tgz' % (self.BASE, course_id.replace('/','+'), sem)
+            r3 = self.ses.get(url)
+
         dt = time.ctime(time.time()).replace(' ','_').replace(':','')
         fn = 'COURSE_DATA/COURSE-%s___%s.tar.gz' % (course_id.replace('/','__'),dt)
     
@@ -63,10 +67,12 @@ class edxStudio(object):
     
         print "Uploading %s for %s" % (tfn, course_id)
     
+        tfnbn = os.path.basename(tfn)
         (org, num, sem) = course_id.split('/')
+        mode = 1
         url = '%s/import/%s/branch/draft/block/%s' % (self.BASE, course_id.replace('/','.'), sem)
     
-        files = {'course-data': (tfn, open(tfn, 'rb'), 'application/x-gzip')}
+        files = {'course-data': (tfnbn, open(tfn, 'rb'), 'application/x-gzip')}
     
         csrf = self.ses.cookies['csrftoken']
         print "csrf=%s" % csrf
@@ -76,6 +82,12 @@ class edxStudio(object):
                }
     
         r2 = self.ses.get(url)
+
+        if not r2.ok or (r2.status_code==404):
+            url = '%s/import/slashes:%s' % (self.BASE, course_id.replace('/','+'))
+            mode = 2
+            r2 = self.ses.get(url)
+
         print r2.status_code
         r3 = self.ses.post(url, files=files, headers=headers)
         # print r3.headers
@@ -84,11 +96,17 @@ class edxStudio(object):
     
         print "--> %s" % (r3.content)
     
-        url = '%s/import_status/%s/branch/draft/block/%s/%s' % (self.BASE, course_id.replace('/','.'), sem, tfn.replace('/','-'))
+        url = '%s/import_status/%s/branch/draft/block/%s/%s' % (self.BASE, course_id.replace('/','.'), sem, tfnbn.replace('/','-'))
+        if mode==2:
+            url = '%s/import_status/slashes:%s/%s' % (self.BASE, course_id.replace('/','+'), tfnbn.replace('/','-'))
+        print url
     
         for k in range(nwait):
             r4 = self.ses.get(url)
-            print r4.content
+            if r4.ok:
+                print r4.content
+            else:
+                print r4
             sys.stdout.flush()
             time.sleep(2)
             
